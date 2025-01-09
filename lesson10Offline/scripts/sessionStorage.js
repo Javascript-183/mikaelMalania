2ed2f1e4953534cebc64138006a1755b347ed98c
Mikael Malania <m.malania@noxtton.com>
2025-01-09 22:35:20 +0400

    a

---
+++ lesson10Offline/scripts/sessionStorage.js
@@ -0,0 +1,11 @@
+sessionStorage.setItem("username", "student123");
+sessionStorage.setItem("password", "ajanjuri123");
+
+sessionStorage.removeItem("password");
+
+console.log(sessionStorage.getItem("username"))
+
+// console.log(localStorage.key(4))
+
+// localStorage.clear
+
